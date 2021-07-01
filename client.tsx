160011852d5621ea01ee7d8837590fa36241d8dc
jjhstoday <jjh.s.today@gmail.com>
2021-07-01 16:20:17 +0900

    feat: 로또추첨기 타이핑(FC, useMemo)

--- client.tsx
+++ client.tsx
@@ -5,8 +5,9 @@ import { hot } from 'react-hot-loader/root';
 // import GuGuDan from './GuGuDan';
 // import NumberBaseball from './NumberBaseball';
 // import ResponseCheck from './ResponseCheck';
-import RSP from './RSP';
+// import RSP from './RSP';
+import Lotto from './Lotto';
 
-const Hot = hot(RSP);
+const Hot = hot(Lotto);
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));
